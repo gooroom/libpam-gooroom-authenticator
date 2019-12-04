cfd929af6c057b5b6ea2d50d3260344c9b1348ce
cjheo <cjheo@gooroom.kr>
2019-12-04 20:17:14 +0900

    Update sources regarding copyright
    Change-Id: I24bb679528858d64229a71128ed9eea87ecc20eb

--- src/nfc_auth.h
+++ src/nfc_auth.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 2017 gooroom <gooroom@gooroom.kr>
+ * Copyright (c) 2015-2019 Gooroom <gooroom@gooroom.kr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
